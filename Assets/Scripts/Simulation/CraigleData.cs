65108daba46d9046b35cdb540ed6afad4b3c7736
Sami Shakkour <Sami.shakkour@hotmail.com>
2026-08-19 21:59:18 +0300

    Craig Data

---
+++ Assets/Scripts/Simulation/CraigleData.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace Craigles
+{
+    public struct CraigleData
+    {
+        public Vector3 Position;
+        public Vector3 Velocity;
+        public float Size;
+        public float Hue; // in degrees from 0 to 360
+        public float Age; // in seconds
+    }
+}
